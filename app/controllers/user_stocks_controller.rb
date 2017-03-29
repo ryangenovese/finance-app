5eb7035c185eae8bcc46088c1b75ba26048f2fb0
Ryan Genovese <ryan.genovese.11@gmail.com>
2017-03-29 14:54:48 +0000

    add user and stock association and display

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -31,7 +31,7 @@ class UserStocksController < ApplicationController
       if stock
         @user_stock = UserStock.new(user: current_user, stock: stock)
       else
-        stock = Stock.new_from_lookup(params: [:stock_ticker])
+        stock = Stock.new_from_lookup(params[:stock_ticker])
         if stock.save
           @user_stock = UserStock.new(user: current_user, stock: stock)
         else
@@ -40,11 +40,10 @@ class UserStocksController < ApplicationController
         end
       end
     end
-    
 
     respond_to do |format|
       if @user_stock.save
-        format.html { redirect_to my_portfolio_path, notice: "Stock #{@user_stock.stock.ticker} stock was successfully added." }
+        format.html { redirect_to my_portfolio_path, notice: "Stock #{@user_stock.stock.ticker} was successfully added" }
         format.json { render :show, status: :created, location: @user_stock }
       else
         format.html { render :new }
@@ -72,7 +71,7 @@ class UserStocksController < ApplicationController
   def destroy
     @user_stock.destroy
     respond_to do |format|
-      format.html { redirect_to user_stocks_url, notice: 'User stock was successfully destroyed.' }
+      format.html { redirect_to my_portfolio_path, notice: 'Stock was successfully removed from portfolio.' }
       format.json { head :no_content }
     end
   end
